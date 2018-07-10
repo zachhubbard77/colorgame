25b74af27c157a08c5f469aa0479f7d00b023e7c
za_git <26679283+zachhubbard77@users.noreply.github.com>
2018-07-10 14:00:14 -0700

    Coded most functionality

--- main.js
+++ main.js
@@ -1,14 +1,127 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var header = document.querySelector("h1");
+var resetButton = document.getElementById("reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+// Easy and Hard modes below. Need to work out easy bug. 
+/*easyBtn.addEventListener('click', function(){
+   hardBtn.classList.remove("selected");
+   easyBtn.classList.add("selected");
+    colors = generateRandomColors(3);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+    
+    
+});
+
+hardBtn.addEventListener('click', function(){
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected"); 
+});*/
+
+
+resetButton.addEventListener('click', function(){
+    // generate all new colors
+    colors = generateRandomColors(6);
+    
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change colorDisplay to match picked color
+    colorDisplay.textContent = pickedColor;
+    //generate colors of squares
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = colors[i];
+    }
+    header.style.backgroundColor = "";
+});
+
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
+    //Add initial colors to squares 
     squares[i].style.backgroundColor = colors[i];
+
+    //add click listener to squares
+    squares[i].addEventListener('click', function(){
+        //grab color of clicked square and compare color to picked color
+        var clickedColor = this.style.backgroundColor;
+        
+        
+        console.log(clickedColor, pickedColor);
+        
+        
+        if(clickedColor !== pickedColor){
+            this.style.backgroundColor = "#232323"; 
+            messageDisplay.textContent = "Try again!"
+            
+        } else {
+            messageDisplay.textContent = "Correct!";
+            resetButton.textContent = "Play Again?"
+            changeColors(clickedColor);
+            header.style.backgroundColor = pickedColor;
+            
+        }
+    });
+    
 }
+
+
+//Function for changing all the colors to right color at once.
+
+function changeColors(color){
+    // loop through all squares. change each color to match given color
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+    
+}
+
+
+function generateRandomColors(num){
+    //make an array
+    var arr = [];
+    //add num random colors to array
+    for(var i = 0; i < num; i++){
+        arr.push(randomColor());
+        //get random color
+        
+        //push into array
+    }
+    //return that array
+    return arr;
+    
+}
+
+function randomColor(){
+    //pick a red from 0 to 255
+    var r = Math.floor(Math.random() * 256);
+    //pick a green from 0 to 255
+    var g = Math.floor(Math.random() * 256);
+    //pick a blue from 0 to 255
+    var b = Math.floor(Math.random() * 256);
+    
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+
+
